5c82b8c0fe0233c8c6ae9a15d9047eb9a770e4e4
Trey Piepmeier <trey@treypiepmeier.com>
2013-09-03 14:11:15 -0400

    dotjs favicon tweaks

---
+++ js/app.e2ma.net.js
@@ -0,0 +1,1 @@
+$('<link rel="shortcut icon" href="//myemma.com/static/global/favicons/favicon.ico?v=2" type="image/x-icon">').appendTo('head');
---
+++ js/myemma.atlassian.net.js
@@ -0,0 +1,1 @@
+$('link[rel*=icon]').attr('href', '//www.atlassian.com/dms/wac/favicon_wac.ico');
