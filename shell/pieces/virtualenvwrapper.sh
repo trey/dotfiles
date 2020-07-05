bfca09571a208435281cbedc67d95d61d5e17b6d
Trey Piepmeier <trey@treypiepmeier.com>
2020-07-05 17:10:01 -0400

    Make virtualenvwrapper happy with zsh.

--- shell/pieces/virtualenvwrapper.sh
+++ shell/pieces/virtualenvwrapper.sh
@@ -1,5 +1,2 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python3'
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    . /usr/local/bin/virtualenvwrapper.sh
-fi
