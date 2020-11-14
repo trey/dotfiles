acfed937d6d4c10adc4b2c066d7e363804bcc5bb
Trey Piepmeier <trey@treypiepmeier.com>
2020-11-14 11:33:39 -0500

    Add scripts that used to live in Dropbox.
    I’m trying to get away from that lifestyle.
    
    Jan 23  2014 gbrt
    Sep  3  2016 git-who
    Jul  2  2018 isreg

--- shell/pieces/env.sh
+++ shell/pieces/env.sh
@@ -3,6 +3,6 @@ export GIT_EDITOR="code --wait"
 export GEM_HOME=$HOME/gems
 export PATH="/usr/local/bin:\
 $HOME/bin:\
-$HOME/bin/common:\
+$HOME/bin/dotfiles/shell/scripts:\
 $HOME/gems/bin:\
 $PATH"
